3a6428c9d0d8b3e03e4d190596a634b60ad45234
Christopher Bradhaw <christopher.bradshaw@clever.com>
2016-08-29 19:22:15 +0000

    Add check that email was sent

--- email/email.go
+++ email/email.go
@@ -29,6 +29,12 @@ func SendEmail(payload *config.Payload, duration time.Duration, res science.Resu
 	templateName := "http-science-results"
 
 	mandrillClient := mandrill.ClientWithKey(os.Getenv("MANDRILL_KEY"))
-	_, err := mandrillClient.MessagesSendTemplate(message, templateName, map[string]string{})
-	return err
+
+	resp, err := mandrillClient.MessagesSendTemplate(message, templateName, nil)
+	if err != nil {
+		return err
+	} else if resp[0].Status != "Sent" {
+		return fmt.Errorf("Email not sent, status: %s, reason: %s", resp[0].Status, resp[0].RejectionReason)
+	}
+	return nil
 }
