9559cf8638409f7e3774f2880717e6495fecc0ad
philippeclever <154480041+philippeclever@users.noreply.github.com>
2024-10-04 10:58:52 -0700

    Update sfncli.mk

--- sfncli.mk
+++ sfncli.mk
@@ -6,7 +6,11 @@ SYSTEM := $(shell uname -a | cut -d" " -f1 | tr '[:upper:]' '[:lower:]')
 SFNCLI_INSTALLED := $(shell [[ -e "bin/sfncli" ]] && bin/sfncli --version)
 # AUTH_HEADER is used to help avoid github ratelimiting
 AUTH_HEADER = $(shell [[ ! -z "${GITHUB_API_TOKEN}" ]] && echo "Authorization: token $(GITHUB_API_TOKEN)")
-SFNCLI_LATEST = $(shell curl -s https://api.github.com/repos/Clever/sfncli/releases/latest  | grep tag_name | cut -d\" -f4)
+SFNCLI_LATEST = $(shell \
+	curl --retry 5 -f -s --header "$(AUTH_HEADER)" \
+		https://api.github.com/repos/Clever/sfncli/releases/latest | \
+	grep tag_name | \
+	cut -d\" -f4)
 
 .PHONY: bin/sfncli sfncli-update-makefile ensure-sfncli-version-set ensure-curl-installed
 
